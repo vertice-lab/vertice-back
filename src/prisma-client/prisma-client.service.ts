0c0f279e817eafc394d29e840c66da174f41ed6e
Sergio Ventura <sergiovforesto@gmail.com>
2026-04-14 20:08:44 -0400

    prisma config change

--- src/prisma-client/prisma-client.service.ts
+++ src/prisma-client/prisma-client.service.ts
@@ -1,13 +1,20 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaPg } from '@prisma/adapter-pg';
 import { PrismaClient } from 'generated/prisma/client';
+import { Pool } from 'pg';
 
 @Injectable()
 export class PrismaClientService extends PrismaClient {
   constructor() {
-    const adapter = new PrismaPg({
+    const pool = new Pool({
       connectionString: process.env.DATABASE_URL,
+      ssl: {
+        rejectUnauthorized: false,
+      },
     });
+
+    const adapter = new PrismaPg(pool);
+
     super({ adapter });
   }
 }
